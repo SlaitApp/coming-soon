df11edb1b6e7e4c494792f6b17d5c3bbcc2f0432
Malcom Green <malcomeqgreen@gmail.com>
2022-08-15 16:09:34 -0400

    Switched to summary_large_image

--- pages/_app.js
+++ pages/_app.js
@@ -19,14 +19,14 @@ function MyApp({ Component, pageProps }) {
     <meta property="og:image:secure_url" content="https://slait.net/og_card.png" />
     <meta property="og:image:type" content="image/png" />
 
-    <meta property="og:image:width" content="1500" />
-    <meta property="og:image:height" content="900" />
-
-    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Slait Language Exchange Platform - Chat with natives in any language" />
     <meta name="twitter:description" content="Slait Language Exchange Platform - Learn a new language, improve your speaking skills by speaking with natives all over the world." />
     <meta name="twitter:image" content="https://slait.net/og_card.png" />
     <meta name="twitter:site" content="@slaitapp" />
+    <meta property="twitter:domain" content="slait.net" />
+    <meta property="twitter:url" content="https://slait.net/" />
+    
   </Head>
   return <Component {...pageProps} />
 }
