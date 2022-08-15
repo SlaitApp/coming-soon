cf208ae061f9c4191da47edef82e12ea79fa717c
Malcom Green <malcomeqgreen@gmail.com>
2022-08-15 15:31:03 -0400

    Adjusted meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -22,11 +22,11 @@ function MyApp({ Component, pageProps }) {
     <meta property="og:image:width" content="1500" />
     <meta property="og:image:height" content="900" />
 
-    <meta property="twitter:card" content="summary" />
-    <meta property="twitter:title" content="Slait Language Exchange Platform - Chat with natives in any language" />
-    <meta property="twitter:description" content="Slait Language Exchange Platform - Learn a new language, improve your speaking skills by speaking with natives all over the world." />
-    <meta property="twitter:image" content="https://slait.net/og_card.png" />
-    <meta property="twitter:site" content="@slaitapp" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Slait Language Exchange Platform - Chat with natives in any language" />
+    <meta name="twitter:description" content="Slait Language Exchange Platform - Learn a new language, improve your speaking skills by speaking with natives all over the world." />
+    <meta name="twitter:image" content="https://slait.net/og_card.png" />
+    <meta name="twitter:site" content="@slaitapp" />
   </Head>
   return <Component {...pageProps} />
 }
