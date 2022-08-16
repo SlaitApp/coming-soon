922f1faf43e0d17c531ab8138caadd9da1b6378d
Malcom Green <malcomeqgreen@gmail.com>
2022-08-16 19:52:20 -0400

    Proper Sentry integration

--- next.config.wizardcopy.js
+++ next.config.wizardcopy.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const moduleExports = {
+  // Your existing module.exports
+};
 
 const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
@@ -18,4 +21,6 @@ const sentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
